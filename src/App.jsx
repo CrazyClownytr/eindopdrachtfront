4ece726b71cb540ea366487441466b4f491ccba7
crazyclownytr <yashtr02@gmail.com>
2025-01-26 12:12:21 +0100

    add url error message

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PlayerDetail from "./components/PlayerDetails.jsx";
 import Layout from "./components/Layout.jsx";
 import UpdatePlayer from "./components/UpdatePlayer.jsx";
 import DeletePlayer from "./components/DeletePlayer.jsx";
+import NotFoundError from "./components/NotFoundError.jsx";
 
 const router = createBrowserRouter([
     {
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
                 path: '/',
                 element: <PlayerList />,
             },
+
             {
                 path: '/create',
                 element: <CreatePlayer />,
@@ -32,6 +34,11 @@ const router = createBrowserRouter([
             {
                 path: '/player/delete/:id',
                 element: <DeletePlayer />,
+            },
+
+            {
+                path: '*',
+                element: <NotFoundError />,
             },
 
 
---
+++ src/components/NotFoundError.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <div>
+            <h2>404 - Pagina niet gevonden</h2>
+            <p>De pagina die je zoekt bestaat niet.</p>
+        </div>
+    );
+}
+
+export default NotFound;
